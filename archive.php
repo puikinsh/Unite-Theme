e28b081914cc2b7a967fc635bb69a3cbff441ecb
puikinsh <aigars.silkalns@gmail.com>
2015-06-19 13:37:37 +0300

    Major changes in archive.php - more in WP 4.3

--- archive.php
+++ archive.php
@@ -15,65 +15,9 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-						if ( is_category() ) :
-							single_cat_title();
-
-						elseif ( is_tag() ) :
-							single_tag_title();
-
-						elseif ( is_author() ) :
-							printf( __( 'Author: %s', 'unite' ), '<span class="vcard">' . get_the_author() . '</span>' );
-
-						elseif ( is_day() ) :
-							printf( __( 'Day: %s', 'unite' ), '<span>' . get_the_date() . '</span>' );
-
-						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'unite' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'unite' ) ) . '</span>' );
-
-						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'unite' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'unite' ) ) . '</span>' );
-
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'unite' );
-
-						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
-							_e( 'Galleries', 'unite');
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'unite');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'unite' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'unite' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'unite' );
-
-						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
-							_e( 'Statuses', 'unite' );
-
-						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
-							_e( 'Audios', 'unite' );
-
-						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
-							_e( 'Chats', 'unite' );
-
-						else :
-							_e( 'Archives', 'unite' );
-
-						endif;
-					?>
-				</h1>
 				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
--- inc/extras.php
+++ inc/extras.php
@@ -511,3 +511,93 @@ function unite_custom_favicon() {
 endif;
 add_action( 'wp_head', 'unite_custom_favicon', 0 );
 add_action( 'admin_head', 'unite_custom_favicon', 0 );
+
+if ( ! function_exists( 'the_archive_title' ) ) :
+/**
+ * Shim for `the_archive_title()`.
+ *
+ * Display the archive title based on the queried object.
+ *
+ * @todo Remove this function when WordPress 4.3 is released.
+ *
+ * @param string $before Optional. Content to prepend to the title. Default empty.
+ * @param string $after  Optional. Content to append to the title. Default empty.
+ */
+function the_archive_title( $before = '', $after = '' ) {
+  if ( is_category() ) {
+    $title = sprintf( esc_html__( 'Category: %s', 'unite' ), single_cat_title( '', false ) );
+  } elseif ( is_tag() ) {
+    $title = sprintf( esc_html__( 'Tag: %s', 'unite' ), single_tag_title( '', false ) );
+  } elseif ( is_author() ) {
+    $title = sprintf( esc_html__( 'Author: %s', 'unite' ), '<span class="vcard">' . get_the_author() . '</span>' );
+  } elseif ( is_year() ) {
+    $title = sprintf( esc_html__( 'Year: %s', 'unite' ), get_the_date( esc_html_x( 'Y', 'yearly archives date format', 'unite' ) ) );
+  } elseif ( is_month() ) {
+    $title = sprintf( esc_html__( 'Month: %s', 'unite' ), get_the_date( esc_html_x( 'F Y', 'monthly archives date format', 'unite' ) ) );
+  } elseif ( is_day() ) {
+    $title = sprintf( esc_html__( 'Day: %s', 'unite' ), get_the_date( esc_html_x( 'F j, Y', 'daily archives date format', 'unite' ) ) );
+  } elseif ( is_tax( 'post_format' ) ) {
+    if ( is_tax( 'post_format', 'post-format-aside' ) ) {
+      $title = esc_html_x( 'Asides', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) {
+      $title = esc_html_x( 'Galleries', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-image' ) ) {
+      $title = esc_html_x( 'Images', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-video' ) ) {
+      $title = esc_html_x( 'Videos', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-quote' ) ) {
+      $title = esc_html_x( 'Quotes', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-link' ) ) {
+      $title = esc_html_x( 'Links', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-status' ) ) {
+      $title = esc_html_x( 'Statuses', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-audio' ) ) {
+      $title = esc_html_x( 'Audio', 'post format archive title', 'unite' );
+    } elseif ( is_tax( 'post_format', 'post-format-chat' ) ) {
+      $title = esc_html_x( 'Chats', 'post format archive title', 'unite' );
+    }
+  } elseif ( is_post_type_archive() ) {
+    $title = sprintf( esc_html__( 'Archives: %s', 'unite' ), post_type_archive_title( '', false ) );
+  } elseif ( is_tax() ) {
+    $tax = get_taxonomy( get_queried_object()->taxonomy );
+    /* translators: 1: Taxonomy singular name, 2: Current taxonomy term */
+    $title = sprintf( esc_html__( '%1$s: %2$s', 'unite' ), $tax->labels->singular_name, single_term_title( '', false ) );
+  } else {
+    $title = esc_html__( 'Archives', 'unite' );
+  }
+  /**
+   * Filter the archive title.
+   *
+   * @param string $title Archive title to be displayed.
+   */
+  $title = apply_filters( 'get_the_archive_title', $title );
+  if ( ! empty( $title ) ) {
+    echo $before . $title . $after;  // WPCS: XSS OK.
+  }
+}
+endif;
+if ( ! function_exists( 'the_archive_description' ) ) :
+/**
+ * Shim for `the_archive_description()`.
+ *
+ * Display category, tag, or term description.
+ *
+ * @todo Remove this function when WordPress 4.3 is released.
+ *
+ * @param string $before Optional. Content to prepend to the description. Default empty.
+ * @param string $after  Optional. Content to append to the description. Default empty.
+ */
+function the_archive_description( $before = '', $after = '' ) {
+  $description = apply_filters( 'get_the_archive_description', term_description() );
+  if ( ! empty( $description ) ) {
+    /**
+     * Filter the archive description.
+     *
+     * @see term_description()
+     *
+     * @param string $description Archive description to be displayed.
+     */
+    echo $before . $description . $after;  // WPCS: XSS OK.
+  }
+}
+endif;
